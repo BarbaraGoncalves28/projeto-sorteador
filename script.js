df36a830267a2580d86c99b55bbd4ab61b152cc6
BarbaraGoncalves28 <barbaragoncalves2806@gmail.com>
2024-04-09 23:01:19 -0300

    Alterações no arquivo style.css

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const button = document.querySelector(".botaoSortear")
 function sortearValues(){
     const inputMin = parseInt(document.querySelector(".inputMin").value)
     const inputMax = parseInt(document.querySelector(".inputMax").value)
+    
     console.log(inputMin, inputMax)
 
-    const valuesresult = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin
-    console.log(valuesresult) 
+    if(inputMin >= inputMax){
+        alert("O valor mínimo tem que ser MENOR que o valor máximo!")
+    } else {
+        const valuesresult = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin
+        alert(valuesresult) 
+    }
 
 }
 button.addEventListener("click", sortearValues)
